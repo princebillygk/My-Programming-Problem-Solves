7e27b59d1455295b93a511b647c11ab8de4f9db5
Prince BIlly Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2018-06-17 15:49:59 +0600

    অনেক দিন পর আবার শুরু করলাম

---
+++ Light OJ/Light OJ 1072.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#define pi acos(-1)
+using namespace std;
+int main(){
+//    freopen("Output.txt","w",stdout);
+    double R,r,n,Case;
+    cin>>Case;
+    for(int i=1;i<=Case;i++){
+        cin>>R>>n;
+        r=R*sin(pi/n)/(1+sin(pi/n));
+        printf("Case %d: %f\n",i,r);
+    }
+    printf("\n");
+    return 0;
+}
