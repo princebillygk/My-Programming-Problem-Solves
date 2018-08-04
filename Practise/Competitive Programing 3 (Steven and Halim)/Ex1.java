b06cf34f9b99dce2c91fdedd4895c01b8d40fc06
Prince Billy Graham <princebillygrahamkarmoker@gmail.com>
2018-08-04 16:58:37 +0600

    commit

--- Practise/Competitive Programing 3 (Steven and Halim)/Ex1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Ex1{
-	public static void main(String[] args){
-		Scanner sc= new Scanner(System.in);
-		double input =sc.nextDouble();
-		System.out.println("You Entered: "+input);	 
-	}
-}
