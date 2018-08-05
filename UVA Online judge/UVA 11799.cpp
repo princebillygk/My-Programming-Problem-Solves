68e10942f2bee7d452d05cb7d09a618db11c8702
Prince Billy Graham <princebillygrahamkarmoker@gmail.com>
2018-08-05 13:52:15 +0600

    commit

---
+++ UVA Online judge/UVA 11799.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+	int T,N,speed,maxSpeed;
+	scanf("%d",&T);
+	for(int i=0;i<T;i++){
+		maxSpeed=0;
+		scanf("%d",&N);
+		while(N--){
+			scanf("%d",&speed);
+			if(speed>maxSpeed)
+				maxSpeed=speed;
+		}
+		printf("Case %d: %d\n",i,maxSpeed);
+	}
+	return 0;
+}
