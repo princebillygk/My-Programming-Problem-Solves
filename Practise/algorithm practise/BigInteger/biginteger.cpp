0f3d4c53e4b29b5c77b16a6cc046f5130853bd2e
Prince Billy Graham <princebillygrahamkarmoker@hotmail.com>
2019-08-24 00:09:50 +0600

    furnishing bigInt

--- Practise/algorithm practise/BigInteger/biginteger.cpp
+++ Practise/algorithm practise/BigInteger/biginteger.cpp
@@ -19,24 +19,27 @@ int main(){
 		maxlen=len1>len2? len1: len2;
 
 		//strtoarr
-		for(int i=0; i<maxlen+1; i++){
-			if(i>=len1)
+		//the string is converting to array in reverse order
+		for(int i=0; i<maxlen+1; i++){ //adding an extra 0 for safety
+			if(i>=len1) // adding 0 to front if it is sorter than the other
 				a[i]=0;
 			else 
-				a[i]=str1[len1-1-i]-'0';
-		
+				a[i]=str1[len1-1-i]-'0'; //converting character to string reverse order
+			
+			//same process for next varriable
 			if(i>=len2)
-				b[i]=0;
+				b[i]=0; 
 			else 
-				b[i]=str2[len2-1-i]-'0';
+				b[i]=str2[len2-1-i]-'0'; 
 
+			//init
 			reverseRes[i]=0; //init res
 		}
 
 		//addition
 		for(int i=0;i<maxlen;i++){
 			reverseRes[i]+=a[i]+b[i];
-			if(reverseRes[i]>=10){
+			if(reverseRes[i]>=10){ //if carry
 				reverseRes[i]-=10;
 				reverseRes[i+1]++;
 			}
@@ -45,11 +48,12 @@ int main(){
 		//reversing the result to get the accurate form
 		printf("Addition: ");
 		reslen=0;
-		//if carry
+		//if the length is increased because of sum
 		if(reverseRes[maxlen]!=0){
 			res[0]=reverseRes[maxlen];
 			reslen++;
-		}
+		}//consider that extra digit in result array
+
 		for(int i=0;i<maxlen;i++){
 			res[reslen]=reverseRes[maxlen-1-i];
 			reslen++;
@@ -66,8 +70,8 @@ int main(){
 			reverseRes[i]=a[i];
 		}//init res
 		for(int i=0;i<maxlen;i++){
-			if(reverseRes[i]<b[i]){
-				reverseRes[i]=reverseRes[i]+10-b[i];
+			if(reverseRes[i]<b[i]){ //if needed to borrow
+				reverseRes[i]=reverseRes[i]+10-b[i]; 
 				reverseRes[i+1]--;
 			}else{
 				reverseRes[i]=reverseRes[i]-b[i];
@@ -78,7 +82,7 @@ int main(){
 		reslen=0;
 		if(reverseRes[maxlen-1]==0){
 			reslen--;
-		}
+		}//ignoring that extra digit 0
 		
 		//reversing the result
 		printf("Substraction: ");
@@ -91,6 +95,7 @@ int main(){
 		for(int i=0;i<reslen;i++){
 			printf("%d",res[i]);
 		}
+
 		printf("\n");
 		for(int i=0;i<len1+len2;i++){
 				reverseRes[i]=0;
@@ -109,13 +114,17 @@ int main(){
 			}
 		}		
 
-		//if no carry len1+len2-1	
+		//max length for multiplication will be
+		//len1+len2
+		//min lenght for multiplication will be
+		//len1+len2-1
+
+		//if the length is len1+len2-1	
 		reslen=0;
-		//*/
 		if(reverseRes[len1+len2-1]==0){
 			reslen--;
-		}
-
+		}//ignoring that extra 0;
+		//else the length is len1+len2
 		//reversing the result
 		for(int i=0;i<len1+len2;i++){
 			res[reslen]=reverseRes[len1+len2-i-1];
