e69635fa22b53e4e2be2f08f09345d81a4b2f129
Prince Billy Graham <princebillygrahamkarmoker@hotmail.com>
2019-08-23 23:57:58 +0600

    learning big integer

---
+++ Hacker Rank/Interview Preperation/Arrays/New Year Chaos.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumBribes function below.
+void minimumBribes(vector<int> q) {
+    long long  count=0,shift, n=q.size();
+    for (long long i=0;i<n;i++){
+        shift=q[i]-(i+1);
+        if(shift>2){
+            printf("Too chaotic\n");
+            return;
+        }
+        else if (shift<1)
+            continue;
+        else count+=shift;
+    }
+    printf("%lld\n",count);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split_string(q_temp_temp);
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
---
+++ Practise/algorithm practise/BigInteger/biginteger.cpp
@@ -0,0 +1,137 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	//*/
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	//*/
+
+	char str1[400],str2[400];
+	int temp,a[400],b[400],reverseRes[400],res[400], reslen, len1, len2, maxlen;
+
+	while(scanf("%s %s",str1,str2)!=EOF){
+
+		//length calculation
+		len1= strlen(str1);
+		len2= strlen(str2);
+		maxlen=len1>len2? len1: len2;
+
+		//strtoarr
+		for(int i=0; i<maxlen+1; i++){
+			if(i>=len1)
+				a[i]=0;
+			else 
+				a[i]=str1[len1-1-i]-'0';
+		
+			if(i>=len2)
+				b[i]=0;
+			else 
+				b[i]=str2[len2-1-i]-'0';
+
+			reverseRes[i]=0; //init res
+		}
+
+		//addition
+		for(int i=0;i<maxlen;i++){
+			reverseRes[i]+=a[i]+b[i];
+			if(reverseRes[i]>=10){
+				reverseRes[i]-=10;
+				reverseRes[i+1]++;
+			}
+		}
+
+		//reversing the result to get the accurate form
+		printf("Addition: ");
+		reslen=0;
+		//if carry
+		if(reverseRes[maxlen]!=0){
+			res[0]=reverseRes[maxlen];
+			reslen++;
+		}
+		for(int i=0;i<maxlen;i++){
+			res[reslen]=reverseRes[maxlen-1-i];
+			reslen++;
+		}
+		
+		//printing result for addition
+		for(int i=0;i<reslen;i++){
+			printf("%d",res[i]);
+		}
+		printf("\n");		
+
+		//substraction
+		for(int i=0;i<maxlen;i++){
+			reverseRes[i]=a[i];
+		}//init res
+		for(int i=0;i<maxlen;i++){
+			if(reverseRes[i]<b[i]){
+				reverseRes[i]=reverseRes[i]+10-b[i];
+				reverseRes[i+1]--;
+			}else{
+				reverseRes[i]=reverseRes[i]-b[i];
+			}
+		}
+
+		//if length decreased		
+		reslen=0;
+		if(reverseRes[maxlen-1]==0){
+			reslen--;
+		}
+		
+		//reversing the result
+		printf("Substraction: ");
+		for(int i=0;i<maxlen;i++){
+			res[reslen]=reverseRes[maxlen-1-i];
+			reslen++;
+		}
+
+		//printing result for substraction
+		for(int i=0;i<reslen;i++){
+			printf("%d",res[i]);
+		}
+		printf("\n");
+		for(int i=0;i<len1+len2;i++){
+				reverseRes[i]=0;
+		}
+
+		//multiplication 
+		for(int i=0;i<len1;i++){
+			for(int j=0;j<len2;j++){
+				reverseRes[i+j]+=a[i]*b[j];
+			}
+		}
+		for(int i=0;i<len1+len2;i++){
+			if(reverseRes[i]>=10){
+				reverseRes[i+1]+=reverseRes[i]/10;
+				reverseRes[i]=reverseRes[i]%10;
+			}
+		}		
+
+		//if no carry len1+len2-1	
+		reslen=0;
+		//*/
+		if(reverseRes[len1+len2-1]==0){
+			reslen--;
+		}
+
+		//reversing the result
+		for(int i=0;i<len1+len2;i++){
+			res[reslen]=reverseRes[len1+len2-i-1];
+			reslen++;
+		}
+
+		//printf the result for multiplication
+		printf("Multiplication: ");
+		for(int i=0;i<reslen;i++){
+			printf("%d",res[i]);
+		}
+		printf("\n\n");
+
+	}
+	
+
+
+	return 0;
+}
