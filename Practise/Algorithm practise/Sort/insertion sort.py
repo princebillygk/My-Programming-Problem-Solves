d48336f599a69dd9e5ed39b64718d9c81d23d0f9
Prince Billy Graham Karmoker <princebillygrahamkarmoker@hotmail.com>
2020-01-08 04:49:46 +0600

    Create insertion sort.py

---
+++ Practise/Algorithm practise/Sort/insertion sort.py
@@ -0,0 +1,27 @@
+a = [];
+#taking input
+print("How many item do you want to enter");
+length = input();
+print("length: "+ str(length));
+for i in range (0, length):
+    a.append(input())
+
+#check
+print("Before Sorted:");
+print(a);
+
+#insertion sort
+for i in range(1, length):
+    key = a[i]
+    for j in range(i - 1, -1, -1):
+        if a[j] > key:
+            a[j + 1] =  a[j]
+            a[j] = key
+            
+#output
+print("After Sorted:");
+print(a);
+
+
+
+
